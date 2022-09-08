526dc4fc6c70741b2ea1bb6d2aee9d35ab4f5e21
soher <soher.vatik@gmail.com>
2022-09-08 11:18:30 +0300

    test

--- src/config.ts
+++ src/config.ts
@@ -56,42 +56,42 @@ export default {
     {
       name: 'L8LIFE White Hoodie',
       model: 'models/wearables/hoodie_female_WHITE.glb',
-      link: 'https://market.decentraland.org/contracts/0x6a6165ac5a213c1d3199e351828f739e09fb48b7/items/1',
+      link: 'https://market.decentraland.org/contracts/0x6a6165ac5a213c1d3199e351828f739e09fb48b7/items/1/',
       position: new Vector3(-4.2, 0, 5.1),
       rotation: Quaternion.Euler(0, 0, 0)
     },
     {
       name: 'L8LIFE White T-Shirt',
       model: 'models/wearables/shirt_female_elevate.glb',
-      link: 'https://market.decentraland.org/contracts/0x6a6165ac5a213c1d3199e351828f739e09fb48b7/items/0',
+      link: 'https://market.decentraland.org/contracts/0x6a6165ac5a213c1d3199e351828f739e09fb48b7/items/0/',
       position: new Vector3(5.1, -0.1, -3.3),
       rotation: Quaternion.Euler(0, -90, 0)
     },
     {
       name: 'Colorful Strapless Heart Dress',
       model: 'models/wearables/dress_texture_01.glb',
-      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/1',
+      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/1/',
       position: new Vector3(-5.1, 0.3, -3.3),
       rotation: Quaternion.Euler(0, 90, 0)
     },
     {
       name: 'Red Eye Dress',
       model: 'models/wearables/dress_tight.glb',
-      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/8',
+      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/8/',
       position: new Vector3(0, 0.3, 0.7),
       rotation: Quaternion.Euler(0, 0, 0)
     },
     {
       name: 'Neon Pink Eye of Fashion',
       model: 'models/wearables/neon_pink_eye.glb',
-      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/3',
+      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/3/',
       position: new Vector3(1, 0.7, -0.7),
       rotation: Quaternion.Euler(0, 45, 0)
     },
     {
       name: 'Neon Aqua Eye of Fashion',
       model: 'models/wearables/neon_aqua_eye.glb',
-      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/2',
+      link: 'https://market.decentraland.org/contracts/0xc047535f6976eaba80a9c6f04af74f66622e55eb/items/2/',
       position: new Vector3(-1, 0.7, -0.7),
       rotation: Quaternion.Euler(0, -45, 0)
     }
